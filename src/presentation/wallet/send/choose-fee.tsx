b7132b3b46cae1ee3dff1f25175e27d6e8cd8d0e
altafan <pietro@vulpem.com>
2021-02-10 16:31:09 +0100

    Fetch listed assets from taxi and do not use tickers

--- src/presentation/wallet/send/choose-fee.tsx
+++ src/presentation/wallet/send/choose-fee.tsx
@@ -18,12 +18,13 @@ import { Address } from '../../../domain/wallet/value-objects';
 import { nextAddressForWallet } from '../../../application/utils/restorer';
 import { browser } from 'webextension-polyfill-ts';
 import {
-  assetInfoByHash,
-  assetInfoByTicker,
   feeAmountFromTx,
+  fetchAssetsFromTaxi,
   fetchTopupFromTaxi,
+  lbtcAssetByNetwork,
   fillTaxiTx,
   utxoMapToArray,
+  formatAmount,
 } from '../../utils';
 
 const feeLevelToSatsPerByte: { [key: string]: number } = {
@@ -40,111 +41,122 @@ const taxiURL: Record<string, string> = {
 const ChooseFee: React.FC = () => {
   const history = useHistory();
   const [{ wallets, app, transaction }, dispatch] = useContext(AppContext);
-  const [feeCurrency, setFeeCurrency] = useState<string>('L-BTC');
-  const [feeLevel, setFeeLevel] = useState<string>('50');
+  const [feeCurrency, setFeeCurrency] = useState<string>('');
+  const [feeLevel, setFeeLevel] = useState<string>('');
   const [taxiTopup, setTaxiTopup] = useState<any>(undefined);
   const [satsPerByte, setSatsPerByte] = useState<number>(0);
   const [unsignedPendingTx, setUnsignedPendingTx] = useState<string>('');
+  const [supportedAssets, setSupportedAssets] = useState<string[]>([]);
   const [isWarningFee] = useState<boolean>(true);
-  const receipientAssetTicker = assetInfoByHash[transaction.asset].ticker;
   const unspents = utxoMapToArray(wallets[0].utxoMap);
 
   useEffect(() => {
-    void (async (): Promise<void> => {
-      if (feeCurrency !== receipientAssetTicker && transaction.feeChangeAddress === '') {
+    if (supportedAssets.length <= 0) {
+      void (async (): Promise<void> => {
         try {
-          const wallet = { ...wallets[0] };
-          wallet.confidentialAddresses.push(Address.create(transaction.changeAddress));
-          const feeChangeAddress = await nextAddressForWallet(wallet, app.network.value, true);
-          dispatch(setFeeChangeAddress(feeChangeAddress));
+          const taxiAssets: string[] = await fetchAssetsFromTaxi(taxiURL[app.network.value]);
+          const supportedAssets: string[] = [lbtcAssetByNetwork(app.network.value)].concat(
+            taxiAssets
+          );
+          setSupportedAssets(supportedAssets);
+          console.log(supportedAssets);
+          setFeeCurrency(supportedAssets[0]);
+          setFeeLevel('50');
         } catch (error) {
           console.log(error);
         }
-      }
-    })();
+      })();
+    }
   });
 
   useEffect(() => {
-    void (async (): Promise<void> => {
-      if (feeCurrency === receipientAssetTicker || transaction.feeChangeAddress !== '') {
-        const changeAddressGetter = (asset: string): any => {
-          if (asset === transaction.asset) {
-            return transaction.changeAddress;
-          }
-          return transaction.feeChangeAddress;
-        };
-        const receipients: RecipientInterface[] = [
-          {
-            asset: transaction.asset,
-            value: transaction.amountInSatoshi,
-            address: transaction.receipientAddress,
-          },
-        ];
-
-        if (feeCurrency === 'L-BTC') {
-          // this check prevents to build a tx in case the fee change address isn't
-          // yet available but needed for the tx.
+    if (supportedAssets.length > 0) {
+      void (async (): Promise<void> => {
+        if (feeCurrency !== transaction.asset && transaction.feeChangeAddress === '') {
           try {
-            const w = walletFromCoins(unspents, app.network.value);
-
-            const currentSatsPerByte = feeLevelToSatsPerByte[feeLevel];
-            if (currentSatsPerByte !== satsPerByte) {
-              const tx: string = w.buildTx(
-                w.createTx(),
-                receipients,
-                greedyCoinSelector(),
-                changeAddressGetter,
-                true,
-                satsPerByte
-              );
-              setUnsignedPendingTx(tx);
-              setSatsPerByte(currentSatsPerByte);
-            }
+            const wallet = { ...wallets[0] };
+            wallet.confidentialAddresses.push(Address.create(transaction.changeAddress));
+            const feeChangeAddress = await nextAddressForWallet(wallet, app.network.value, true);
+            dispatch(setFeeChangeAddress(feeChangeAddress));
           } catch (error) {
             console.log(error);
           }
-        } else {
-          let t = taxiTopup;
-          if (!t) {
-            const feeAsset = assetInfoByTicker[feeCurrency].hash;
-            const topup = await fetchTopupFromTaxi(taxiURL[app.network.value], feeAsset);
-            setTaxiTopup(topup);
-            t = topup;
-          }
-          const taxiPayout = {
-            value: t.topup.assetAmount,
-            asset: t.topup.assetHash,
-            address: '',
-          } as RecipientInterface;
-
-          const tx: string = fillTaxiTx(
-            t.topup.partial,
-            unspents,
-            receipients,
-            taxiPayout,
-            greedyCoinSelector(),
-            changeAddressGetter
-          );
-          setUnsignedPendingTx(tx);
         }
-      }
-    })();
-  }, [
-    feeCurrency,
-    transaction,
-    app,
-    feeLevel,
-    satsPerByte,
-    receipientAssetTicker,
-    taxiTopup,
-    unspents,
-    wallets,
-  ]);
+      })();
+    }
+  });
+
+  useEffect(() => {
+    if (supportedAssets.length > 0) {
+      void (async (): Promise<void> => {
+        if (feeCurrency === transaction.asset || transaction.feeChangeAddress !== '') {
+          const changeAddressGetter = (asset: string): any => {
+            if (asset === transaction.asset) {
+              return transaction.changeAddress;
+            }
+            return transaction.feeChangeAddress;
+          };
+          const receipients: RecipientInterface[] = [
+            {
+              asset: transaction.asset,
+              value: transaction.amountInSatoshi,
+              address: transaction.receipientAddress,
+            },
+          ];
+
+          if (feeCurrency === lbtcAssetByNetwork(app.network.value)) {
+            // this check prevents to build a tx in case the fee change address isn't
+            // yet available but needed for the tx.
+            try {
+              const w = walletFromCoins(unspents, app.network.value);
+
+              const currentSatsPerByte = feeLevelToSatsPerByte[feeLevel];
+              if (currentSatsPerByte !== satsPerByte) {
+                const tx: string = w.buildTx(
+                  w.createTx(),
+                  receipients,
+                  greedyCoinSelector(),
+                  changeAddressGetter,
+                  true,
+                  currentSatsPerByte
+                );
+                setUnsignedPendingTx(tx);
+                setSatsPerByte(currentSatsPerByte);
+              }
+            } catch (error) {
+              console.log(error);
+            }
+          } else {
+            let t = taxiTopup;
+            if (!t) {
+              const topup = await fetchTopupFromTaxi(taxiURL[app.network.value], feeCurrency);
+              setTaxiTopup(topup);
+              t = topup;
+            }
+            const taxiPayout = {
+              value: t.topup.assetAmount,
+              asset: t.topup.assetHash,
+              address: '',
+            } as RecipientInterface;
+
+            const tx: string = fillTaxiTx(
+              t.topup.partial,
+              unspents,
+              receipients,
+              taxiPayout,
+              greedyCoinSelector(),
+              changeAddressGetter
+            );
+            setUnsignedPendingTx(tx);
+          }
+        }
+      })();
+    }
+  }, [feeCurrency, transaction, app, feeLevel, satsPerByte, taxiTopup, unspents, wallets]);
 
   const handleConfirm = () => {
-    const feeAsset = assetInfoByTicker[feeCurrency].hash;
     let feeAmount: number;
-    if (feeCurrency === 'L-BTC') {
+    if (feeCurrency === lbtcAssetByNetwork(app.network.value)) {
       feeAmount = parseFloat(feeAmountFromTx(unsignedPendingTx)) * Math.pow(10, 8);
     } else {
       feeAmount = taxiTopup.topup.assetAmount;
@@ -156,11 +168,11 @@ const ChooseFee: React.FC = () => {
           sendAddress: transaction.receipientAddress,
           sendAsset: transaction.asset,
           sendAmount: transaction.amountInSatoshi,
-          feeAsset: feeAsset,
+          feeAsset: feeCurrency,
           feeAmount: feeAmount,
         }),
         () => {
-          dispatch(setFeeAssetAndAmount(feeAsset, feeAmount));
+          dispatch(setFeeAssetAndAmount(feeCurrency, feeAmount));
           history.push(SEND_CONFIRMATION_ROUTE);
           browser.browserAction.setBadgeText({ text: '1' }).catch((ignore) => ({}));
         },
@@ -169,8 +181,8 @@ const ChooseFee: React.FC = () => {
     );
   };
 
-  const handlePayFeesInLBTC = () => setFeeCurrency('L-BTC');
-  const handlePayFeesInUSDt = () => setFeeCurrency('USDt');
+  const handlePayFeesInLBTC = (e: any, assetHash: string) => setFeeCurrency(assetHash);
+  const handlePayFeesInUSDt = (e: any, assetHash: string) => setFeeCurrency(assetHash);
 
   const warningFee = (
     <div className="flex flex-row gap-2 mt-5">
@@ -191,30 +203,37 @@ const ChooseFee: React.FC = () => {
 
       <div className="w-48 mx-auto border-b-0.5 border-graySuperLight pt-2 mb-6"></div>
 
-      <p className="text-sm font-medium">I pay fee in:</p>
+      {supportedAssets.length <= 0 ? (
+        <div>Loading...</div>
+      ) : (
+        [
+          <p className="text-sm font-medium">I pay fee in:</p>,
+          <div className="flex flex-row justify-center gap-0.5 mx-auto w-11/12 mt-2">
+            <Button
+              className="flex-1"
+              isOutline={feeCurrency === lbtcAssetByNetwork(app.network.value)}
+              onClick={handlePayFeesInLBTC}
+              roundedMd={true}
+              textBase={true}
+              extraData={supportedAssets[0]}
+            >
+              L-BTC
+            </Button>
+            <Button
+              className="flex-1"
+              isOutline={feeCurrency !== lbtcAssetByNetwork(app.network.value)}
+              onClick={handlePayFeesInUSDt}
+              roundedMd={true}
+              textBase={true}
+              extraData={supportedAssets[1]}
+            >
+              USDt
+            </Button>
+          </div>,
+        ]
+      )}
 
-      <div className="flex flex-row justify-center gap-0.5 mx-auto w-11/12 mt-2">
-        <Button
-          className="flex-1"
-          isOutline={feeCurrency === 'L-BTC'}
-          onClick={handlePayFeesInLBTC}
-          roundedMd={true}
-          textBase={true}
-        >
-          L-BTC
-        </Button>
-        <Button
-          className="flex-1"
-          isOutline={feeCurrency === 'USDt'}
-          onClick={handlePayFeesInUSDt}
-          roundedMd={true}
-          textBase={true}
-        >
-          USDt
-        </Button>
-      </div>
-
-      {feeCurrency === 'L-BTC' && (
+      {feeCurrency.length > 0 && feeCurrency === lbtcAssetByNetwork(app.network.value) && (
         <div
           className={cx({
             'track-50': feeLevel === '50',
@@ -241,11 +260,13 @@ const ChooseFee: React.FC = () => {
         </div>
       )}
 
-      {feeCurrency === 'USDt' && (
+      {feeCurrency.length > 0 && feeCurrency !== lbtcAssetByNetwork(app.network.value) && (
         <>
           <div className="flex flex-row items-baseline justify-between mt-12">
             <span className="text-lg font-medium">Fee:</span>
-            <span className="font-regular mr-6 text-base">0.00138 USDt *</span>
+            <span className="font-regular mr-6 text-base">
+              {taxiTopup ? `${formatAmount(taxiTopup.topup.assetAmount)} USDt *` : 'Loading...'}
+            </span>
           </div>
           {isWarningFee && warningFee}
           <p className="text-primary mt-3.5 text-xs font-medium text-left">
@@ -257,7 +278,7 @@ const ChooseFee: React.FC = () => {
       <Button
         className="bottom-20 right-8 absolute"
         onClick={handleConfirm}
-        disabled={unsignedPendingTx === ''}
+        disabled={supportedAssets.length <= 0 || unsignedPendingTx === ''}
       >
         Confirm
       </Button>
