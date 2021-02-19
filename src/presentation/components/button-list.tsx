4e459597e4dd49ba351de9c4d4b59947569f3f45
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-02-19 19:41:56 +0100

    add message for transaction not real

--- src/presentation/components/button-list.tsx
+++ src/presentation/components/button-list.tsx
@@ -14,14 +14,15 @@ const ButtonList: React.FC<Props> = ({ children, title, type }: Props) => {
     <div>
       <h2 className="mt-2 text-lg font-medium text-left text-white">{title}</h2>
       <div className="h-64 overflow-y-scroll">
+        {type === 'transactions' && <p className="text-sm font-small text-center text-white">Psst! These transactions are not real! Coming soon...</p>}
         <div className="py-4 space-y-4">
           {React.Children.count(children) === 0 ? (
             <span className="text-sm font-medium text-white">
               Your transactions will appear here
             </span>
           ) : (
-            children
-          )}
+              children
+            )}
         </div>
       </div>
     </div>
