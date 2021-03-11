4ac724024c7f72ff0a67163e2708a32c857f44b5
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-03-12 00:21:34 +0100

    Add initial injecting machinery

---
+++ src/application/content_script.ts
@@ -0,0 +1,95 @@
+interface LiquidProvider {
+  foo(): string;
+};
+
+if (shouldInjectProvider()) {
+  injectScript(
+    '(' +
+    function () {
+      const marina: LiquidProvider = { foo: () => "bar" };
+
+      (window as Record<string, any>).marina = marina;
+      window.dispatchEvent(new Event('marina#initialized'));
+    }
+    + ')();'
+  );
+};
+
+/**
+ * Determines if the provider should be injected
+ *
+ * @returns {boolean} {@code true} Whether the provider should be injected
+ */
+function shouldInjectProvider() {
+  return (
+    doctypeCheck() &&
+    suffixCheck() &&
+    documentElementCheck()
+  );
+}
+
+/**
+ * Injects a script tag into the current document
+ *
+ * @param {string} content - Code to be executed in the current document
+ */
+function injectScript(content: string) {
+  try {
+    const container = document.head || document.documentElement;
+    const scriptTag = document.createElement('script');
+    scriptTag.setAttribute('async', 'false');
+    scriptTag.textContent = content;
+    container.insertBefore(scriptTag, container.children[0]);
+    container.removeChild(scriptTag);
+  } catch (error) {
+    console.error('Marina: Liquid Provider injection failed.', error);
+  }
+}
+
+/**
+ * Checks the doctype of the current document if it exists
+ *
+ * @returns {boolean} {@code true} if the doctype is html or if none exists
+ */
+function doctypeCheck(): boolean {
+  const { doctype } = window.document;
+  if (doctype) {
+    return doctype.name === 'html';
+  }
+  return true;
+}
+
+/**
+ * Returns whether or not the extension (suffix) of the current document is prohibited
+ *
+ * This checks {@code window.location.pathname} against a set of file extensions
+ * that we should not inject the provider into. This check is indifferent of
+ * query parameters in the location.
+ *
+ * @returns {boolean} whether or not the extension of the current document is prohibited
+ */
+function suffixCheck(): boolean {
+  const prohibitedTypes = [/\.xml$/u, /\.pdf$/u];
+  const currentUrl = window.location.pathname;
+  for (let i = 0; i < prohibitedTypes.length; i++) {
+    if (prohibitedTypes[i].test(currentUrl)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * Checks the documentElement of the current document
+ *
+ * @returns {boolean} {@code true} if the documentElement is an html node or if none exists
+ */
+function documentElementCheck(): boolean {
+  const documentElement = document.documentElement.nodeName;
+  if (documentElement) {
+    return documentElement.toLowerCase() === 'html';
+  }
+  return true;
+}
+
+export { }
