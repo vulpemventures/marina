9965960e4769c861e4a88013584e863744e96784
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-03-27 17:57:40 +0100

    spend

--- src/presentation/connect/spend.tsx
+++ src/presentation/connect/spend.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useContext, useEffect, useState } from 'react';
 import Button from '../components/button';
-import Broker from '../../application/content-script';
 import ShellConnectPopup from '../components/shell-connect-popup';
 import { AppContext } from '../../application/store/context';
 import { formatAddress } from '../utils';
@@ -15,11 +14,11 @@ const ConnectSpend: React.FC = () => {
   const [error, setError] = useState<string>('');
   const [tx, setTx] = useState<
     | {
-        hostname?: string;
-        amount?: string;
-        assetHash?: string;
-        recipient?: string;
-      }
+      hostname?: string;
+      amount?: string;
+      assetHash?: string;
+      recipient?: string;
+    }
     | undefined
   >(undefined);
 
@@ -47,17 +46,18 @@ const ConnectSpend: React.FC = () => {
   };
 
   const handleUnlock = async (password: string) => {
-    if (!password || password.length === 0) {
-      setError('Password cannot be empty');
+    if (!password || password.length === 0)
       return;
-    }
 
     try {
       await windowProxy.proxy('SEND_TRANSACTION_RESPONSE', [true, password]);
       window.close();
-    } catch (e) {
+    } catch (e: any) {
       console.error(e);
+      setError(e.message);
     }
+
+    handleModalUnlockClose();
   };
 
   const debouncedHandleUnlock = useRef(
@@ -69,31 +69,42 @@ const ConnectSpend: React.FC = () => {
       className="h-popupContent container pb-20 mx-auto text-center bg-bottom bg-no-repeat"
       currentPage="Spend"
     >
-      <h1 className="mt-8 text-2xl font-medium break-all">{tx?.hostname}</h1>
+      {
+        error.length === 0 ?
+          <>
+            <h1 className="mt-8 text-2xl font-medium break-all">{tx?.hostname}</h1>
 
-      <p className="mt-4 text-base font-medium">Requests you to spend</p>
+            <p className="mt-4 text-base font-medium">Requests you to spend</p>
 
-      <div className="container flex justify-between mt-16">
-        <span className="text-lg font-medium">{tx?.amount}</span>
-        <span className="text-lg font-medium">{assetTicker}</span>
-      </div>
+            <div className="container flex justify-between mt-16">
+              <span className="text-lg font-medium">{tx?.amount}</span>
+              <span className="text-lg font-medium">{assetTicker}</span>
+            </div>
 
-      <div className="container flex items-baseline justify-between mt-4">
-        <span className="mr-2 text-lg font-medium">To: </span>
-        <span className="font-small text-sm break-all">{formatAddress(tx?.recipient ?? '')}</span>
-      </div>
+            <div className="container flex items-baseline justify-between mt-4">
+              <span className="mr-2 text-lg font-medium">To: </span>
+              <span className="font-small text-sm break-all">{formatAddress(tx?.recipient ?? '')}</span>
+            </div>
 
-      <div className="bottom-24 container absolute right-0 flex justify-between">
-        <Button isOutline={true} onClick={handleReject} textBase={true}>
-          Reject
-        </Button>
-        <Button onClick={handleUnlockModalOpen} textBase={true}>
-          Accept
-        </Button>
-      </div>
-
+            <div className="bottom-24 container absolute right-0 flex justify-between">
+              <Button isOutline={true} onClick={handleReject} textBase={true}>
+                Reject
+              </Button>
+              <Button onClick={handleUnlockModalOpen} textBase={true}>
+                Accept
+              </Button>
+            </div>
+          </> :
+          <>
+            <h1 className="mt-8 text-lg font-medium">Oops, Something went wrong...</h1>
+            <p className="mt-4 text-small font-medium">{error}</p>
+            <img className="my-10 mx-auto" src="/assets/images/cross.svg" alt="error" />
+            <Button className="w-36 container mx-auto mt-10" onClick={handleUnlockModalOpen} textBase={true}>
+              Unlock
+            </Button>
+          </>
+      }
       <ModalUnlock
-        error={error}
         isModalUnlockOpen={isModalUnlockOpen}
         handleModalUnlockClose={handleModalUnlockClose}
         handleUnlock={debouncedHandleUnlock}
