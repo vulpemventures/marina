8d9045e12f7e62fe84612f6157b56ed275e8c71e
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-05-07 13:16:08 +0400

    blockstream icon (#168)
    * Update lightnite assets
    
    * Add blockstream asset hash and Logo
    
    * Add Tether droplet icon and show in the transaction page
    
    * prettier

--- src/application/utils/constants.ts
+++ src/application/utils/constants.ts
@@ -1,5 +1,6 @@
 import { Network } from '../../domain/app/value-objects';
 import lightniteAssetsHashes from '../constants/lightnite_asset_hash.json';
+import blockstreamAssetHashes from '../constants/blockstream_asset_hash.json';
 
 export const feeLevelToSatsPerByte: { [key: string]: number } = {
   '0': 0.1,
@@ -21,7 +22,7 @@ const featuredAssets: Record<string, string> = {
   '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d':
     'assets/images/liquid-assets/liquid-btc.svg',
   ce091c998b83c78bb71a632313ba3760f1763d9cfcffae02258ffa9865a37bd2:
-    'assets/images/liquid-assets/liquid-tether.png',
+    'assets/images/liquid-assets/liquid-tether.svg',
   '0e99c1a6da379d1f4151fb9df90449d40d0608f6cb33a5bcbfc8c265f42bab0a':
     'assets/images/liquid-assets/liquid-cad.png',
   '': 'assets/images/liquid-assets/question-mark.svg',
@@ -32,14 +33,20 @@ lightniteAssetsHashes.forEach((assetHash: string) => {
   lightniteAssets[assetHash] = 'assets/images/liquid-assets/lightnite.png';
 });
 
+const blockstreamAssets: Record<string, string> = {};
+blockstreamAssetHashes.forEach((assetHash: string) => {
+  blockstreamAssets[assetHash] = 'assets/images/liquid-assets/blockstream.png';
+});
+
 export const imgPathMapMainnet: Record<string, string> = {
   ...featuredAssets,
   ...lightniteAssets,
+  ...blockstreamAssets,
 };
 
 export const imgPathMapRegtest: Record<string, string> = {
   'L-BTC': 'assets/images/liquid-assets/liquid-btc.svg',
-  USDt: 'assets/images/liquid-assets/liquid-tether.png',
+  USDt: 'assets/images/liquid-assets/liquid-tether.svg',
   LCAD: 'assets/images/liquid-assets/liquid-cad.png',
   '': 'assets/images/liquid-assets/question-mark.svg',
 };
