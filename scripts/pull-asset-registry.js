34d8b41c9d146b93e32282ae595061aaab1bcba4
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-05-07 12:52:08 +0400

    Add blockstream asset hash and Logo

--- scripts/pull-asset-registry.js
+++ scripts/pull-asset-registry.js
@@ -1,22 +1,37 @@
 const fs = require('fs');
 const axios = require('axios');
 
+const BASE_PATH = './src/application/constants';
+
 async function main() {
   try {
     const res = await axios.get('https://assets.blockstream.info/');
 
     // Light Nite assets
-    const assetHashes = Object.values(res.data)
+    const lightNiteAssetHashes = Object.values(res.data)
       .filter(asset => asset.entity.domain.includes("lightnite.io"))
       .map(asset => asset.asset_id);
 
-    const path = './src/application/constants/lightnite_asset_hash.json';
-    fs.writeFileSync(path, JSON.stringify(assetHashes, undefined, 0));
-    console.log('JSON written to ' + path);
+    const lightNitePath = `${BASE_PATH}/lightnite_asset_hash.json`;
+    writeToFile(lightNitePath, lightNiteAssetHashes);
+
+    // Blockstream
+    const blockstreamAssetHashes = Object.values(res.data)
+      .filter(asset => asset.entity.domain.includes("blockstream.com"))
+      .map(asset => asset.asset_id);
+
+    const blockstreamPath = `${BASE_PATH}/blockstream_asset_hash.json`;
+    writeToFile(blockstreamPath, blockstreamAssetHashes);
+
 
   } catch (e) {
     console.error(e)
   }
+}
+
+function writeToFile(path, assets) {
+  fs.writeFileSync(path, JSON.stringify(assets, undefined, 2));
+  console.log('JSON written to ' + path);
 }
 
 main()
