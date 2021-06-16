5918da7eef25bc0866afd5547a91d8dc104435c9
louisinger <louis@vulpem.com>
2021-06-16 17:52:40 +0200

    bip39.validateMnemonic in Mnemonic domain

--- src/domain/mnemonic.ts
+++ src/domain/mnemonic.ts
@@ -1,5 +1,5 @@
-import { IdentityOpts, IdentityType, Mnemonic as MnemonicIdentity, MnemonicOpts } from 'ldk';
 import { Network } from './network';
+import { validateMnemonic } from 'bip39';
 
 export type Mnemonic = string;
 
@@ -7,15 +7,7 @@ export function createMnemonic(mnemo: string, network: Network): Mnemonic {
   // Trim start-end and replace multiple spaces in between with a single space
   const mnemonic = mnemo.trim().replace(/ +(?= )/g, '');
 
-  try {
-    // Mnemonic validation, network doesn't matter
-    new MnemonicIdentity({
-      chain: network,
-      type: IdentityType.Mnemonic,
-      opts: { mnemonic },
-    } as IdentityOpts<MnemonicOpts>);
-    return mnemonic;
-  } catch (err) {
-    throw new Error(err.message);
-  }
+  // Mnemonic validation, network doesn't matter
+  if (!validateMnemonic(mnemonic)) throw new Error('Invalid mnemonic');
+  return mnemonic;
 }
