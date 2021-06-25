4da2781094314965f762d95b929ade77e2362290
louisinger <louis@vulpem.com>
2021-06-25 16:53:27 +0200

    remove comment

--- src/domain/mnemonic.ts
+++ src/domain/mnemonic.ts
@@ -7,7 +7,6 @@ export function createMnemonic(mnemo: string, network: Network): Mnemonic {
   // Trim start-end and replace multiple spaces in between with a single space
   const mnemonic = mnemo.trim().replace(/ +(?= )/g, '');
 
-  // Mnemonic validation, network doesn't matter
   if (!validateMnemonic(mnemonic)) throw new Error('Invalid mnemonic');
   return mnemonic;
 }
