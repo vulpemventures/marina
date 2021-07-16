2e9240754898ee4ee4d307943a2dda8fcf920120
louisinger <louis@vulpem.com>
2021-07-16 09:08:08 +0200

    rewrite cypress test

--- cypress/integration/api.spec.ts
+++ cypress/integration/api.spec.ts
@@ -3,15 +3,19 @@ import { MarinaProvider } from 'marina-provider';
 
 const extension = require('@poziworld/cypress-browser-extension-plugin/helpers')();
 
-// @ts-ignore
-const injectedMarina = window.marina as MarinaProvider;
-
 describe('My First Test', () => {
   it('Does not do much!', async () => {
     const url = 'https://louisinger.github.io/marina-api-test-app/';
     cy.visit(url);
+    cy.get('button').click();
+    cy.wait(2000);
 
-    const isEnabled = await injectedMarina.isEnabled();
-    expect(isEnabled).to.equal(true);
+    cy.get('.App')
+      .find('.success')
+      .should('have.length', 10)
+
+    cy.get('.App')
+      .find('.error')
+      .should('have.length', 0)
   });
 });
