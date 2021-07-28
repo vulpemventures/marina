d99be3667820ec8650a8c2a398f12667b199fe61
louisinger <louis@vulpem.com>
2021-07-28 18:08:14 +0200

    fix webpack.cypress.js

--- webpack.cypress.js
+++ webpack.cypress.js
@@ -1,10 +1,17 @@
 // @ts-nocheck
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin'); 
+const { EnvironmentPlugin } = require('webpack');
 
 module.exports = merge(common, {
-  mode: 'test',
+  mode: 'none',
+  plugins: [
+    new EnvironmentPlugin({
+      NODE_ENV: 'test',
+      NETWORK: 'regtest',
+    })
+  ],
   optimization: {
     minimize: true,
     minimizer: [
