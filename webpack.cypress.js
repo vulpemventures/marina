d73cdbac6ccfbeba6edc88f9981d12e8f7de4b1a
louisinger <louis@vulpem.com>
2021-07-28 17:54:11 +0200

    cypress webpack config

---
+++ webpack.cypress.js
@@ -0,0 +1,22 @@
+// @ts-nocheck
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'test',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+          output: { 
+             ascii_only: true 
+          },
+        },
+      }),
+    ],
+  },
+});
