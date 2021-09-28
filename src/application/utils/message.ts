0416d492a8cfd3c6278f10088200d62499bd355d
Louis Singer <41042567+louisinger@users.noreply.github.com>
2021-09-28 16:29:33 +0200

    bump marina-provider and return public key in SignedMessage (#229)

--- src/application/utils/message.ts
+++ src/application/utils/message.ts
@@ -2,12 +2,13 @@ import * as bip32 from 'bip32';
 import * as bip39 from 'bip39';
 import { networks, payments } from 'ldk';
 import * as bitcoinMessage from 'bitcoinjs-message';
+import { SignedMessage } from 'marina-provider';
 
 export async function signMessageWithMnemonic(
   message: string,
   mnemonic: string,
   network: networks.Network
-): Promise<{ signature: string; address: string }> {
+): Promise<SignedMessage> {
   const seed = await bip39.mnemonicToSeed(mnemonic);
   const node = bip32.fromSeed(seed, network);
   const child = node.derivePath("m/84'/0'/0'/0/0");
@@ -16,5 +17,9 @@ export async function signMessageWithMnemonic(
   });
 
   const pay = payments.p2wpkh({ pubkey: child.publicKey, network });
-  return { signature: signature.toString('base64'), address: pay.address! };
+  return {
+    signature: signature.toString('base64'),
+    address: pay.address!,
+    publicKey: child.publicKey.toString('hex'),
+  };
 }
