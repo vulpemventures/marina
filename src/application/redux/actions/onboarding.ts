3243fd7043b075dfe8352f483686b5c4a3d9dcde
louisinger <louis@vulpem.com>
2021-12-10 12:38:21 +0100

    document redux/actions/setOnboardingVerified

--- src/application/redux/actions/onboarding.ts
+++ src/application/redux/actions/onboarding.ts
@@ -28,6 +28,11 @@ export function setBackup(mnemonic: string): AnyAction {
   };
 }
 
+// Onboarding Verified is a temporary state for verification of the secret mnemonic
+// It aims to flag the new wallet as verified if 
+// (1) the user has already backed up the mnemonic via onboarding/wallet-confirm
+// (2) the user is restoring a wallet via onboarding/wallet-restore
+// then `wallet.isVerified` is set to `onboarding.verified` during onboarding /end-of-flow confirm step
 export function setOnboardingVerified(): AnyAction {
   return {
     type: ONBOARDING_SET_VERIFIED,
