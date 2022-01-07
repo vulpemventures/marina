d4a88dd528946fc65c6e5226308ff3f8d1fe6190
Louis Singer <41042567+louisinger@users.noreply.github.com>
2022-01-07 14:44:31 +0100

    Start periodic updater at background startup (#276)
    * remove console.log
    
    * start periodic updater on background startup

--- src/presentation/wallet/log-in/index.tsx
+++ src/presentation/wallet/log-in/index.tsx
@@ -6,7 +6,7 @@ import { DEFAULT_ROUTE, INITIALIZE_WELCOME_ROUTE } from '../../routes/constants'
 import Button from '../../components/button';
 import Input from '../../components/input';
 import { useDispatch, useSelector } from 'react-redux';
-import { logIn, startPeriodicUpdate } from '../../../application/redux/actions/app';
+import { logIn } from '../../../application/redux/actions/app';
 import { setIdleAction } from '../../../application/utils';
 import {
   AUTHENTICATION_SUCCESS,
@@ -76,7 +76,6 @@ const LogInEnhancedForm = withFormik<LogInFormProps, LogInFormValues>({
       .then(() => {
         if (logInAction.type === AUTHENTICATION_SUCCESS) {
           props.dispatch(updateTaxiAssets()).catch(console.error);
-          props.dispatch(startPeriodicUpdate()).catch(console.error);
           props.history.push(DEFAULT_ROUTE);
           setIdleAction(() => {
             props.dispatch({ type: LOGOUT_SUCCESS }).catch(console.error);
