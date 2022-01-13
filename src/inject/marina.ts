32cfebe543e4cdbf45b7e03d1033feb372e7300b
João Bordalo <joao.bordalo@gmail.com>
2022-01-13 17:23:03 +0000

    reverts to TransactionID

--- src/inject/marina.ts
+++ src/inject/marina.ts
@@ -8,7 +8,7 @@ import {
   Balance,
   MarinaEventType,
   Recipient,
-  TransactionHash,
+  TransactionID,
 } from 'marina-provider';
 import MarinaEventHandler from './marinaEventHandler';
 import WindowProxy from './proxy';
@@ -63,7 +63,7 @@ export default class Marina extends WindowProxy implements MarinaProvider {
     return this.proxy(this.blindTransaction.name, [psetBase64]);
   }
 
-  sendTransaction(recipients: Recipient[], feeAssetHash?: string): Promise<TransactionHash> {
+  sendTransaction(recipients: Recipient[], feeAssetHash?: string): Promise<TransactionID> {
     if (!recipients || !Array.isArray(recipients) || recipients.length === 0) {
       throw new Error('invalid recipients array');
     }
