517dfbfce61ab2522869cd090b92fd829ab57d9f
João Bordalo <joao.bordalo@gmail.com>
2022-02-01 18:28:40 +0000

    raises post to explora error to UI

--- src/application/utils/network.ts
+++ src/application/utils/network.ts
@@ -1,14 +1,24 @@
-import axios from 'axios';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import axios, { AxiosError } from 'axios';
 import { networks, NetworkString } from 'ldk';
 import { IAssets } from '../../domain/assets';
+import { extractErrorMessage } from '../../presentation/utils/error';
 
 export const broadcastTx = async (baseUrl: string, txHex: string): Promise<string> => {
-  const response = await axios.post(`${baseUrl}/tx`, txHex);
-  if (response.status !== 200) {
-    console.error(response.data);
-    throw new Error(response.data);
+  try {
+    const response = await axios.post(`${baseUrl}/tx`, txHex);
+    if (response.status !== 200) {
+      console.error(response.data);
+      throw new Error(response.data);
+    }
+    return response.data;
+  } catch (error: unknown | AxiosError) {
+    console.log(error);
+    if (axios.isAxiosError(error) && error?.response?.data) {
+      throw new Error(error.response.data);
+    }
+    throw new Error(extractErrorMessage(error));
   }
-  return response.data;
 };
 
 export const lbtcAssetByNetwork = (net: NetworkString): string => {
