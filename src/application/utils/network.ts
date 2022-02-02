12158566d3243700c4f07411d5e95e87dc8a8269
João Bordalo <bordalix@users.noreply.github.com>
2022-02-02 10:56:45 +0000

    raises post to explora error to UI (#299)
    * raises post to explora error to UI
    
    * small code refactor:
    - removes console.log() since error is already raised to the UI
    - moves inteilligence about the error to extractErrorMessage()
    
    * prettier

--- src/application/utils/network.ts
+++ src/application/utils/network.ts
@@ -1,14 +1,17 @@
-import axios from 'axios';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import axios, { AxiosError } from 'axios';
 import { networks, NetworkString } from 'ldk';
 import { IAssets } from '../../domain/assets';
+import { extractErrorMessage } from '../../presentation/utils/error';
 
 export const broadcastTx = async (baseUrl: string, txHex: string): Promise<string> => {
-  const response = await axios.post(`${baseUrl}/tx`, txHex);
-  if (response.status !== 200) {
-    console.error(response.data);
-    throw new Error(response.data);
+  try {
+    const response = await axios.post(`${baseUrl}/tx`, txHex);
+    if (response.status !== 200) throw new Error(response.data);
+    return response.data;
+  } catch (error: unknown | AxiosError) {
+    throw new Error(extractErrorMessage(error));
   }
-  return response.data;
 };
 
 export const lbtcAssetByNetwork = (net: NetworkString): string => {
--- src/presentation/utils/error.ts
+++ src/presentation/utils/error.ts
@@ -1,8 +1,25 @@
+import axios from 'axios';
+
 const DEFAULT_ERROR_MSG = 'Unknown error';
 
+/**
+ * Extract a string from the unknown error param to show in the UI
+ * @param error unknown or AxiosError type object
+ * @param defaultMsg optional default message
+ * @returns a string
+ */
 export function extractErrorMessage(error: unknown, defaultMsg: string = DEFAULT_ERROR_MSG) {
+  // if is already a string, return it
   if (typeof error === 'string') return error;
 
+  // since AxiosError is an instance of Error, this should come first
+  if (axios.isAxiosError(error)) {
+    if (error.response) return error.response.data;
+    if (error.request) return error.request.data;
+  }
+
+  // this should be last
   if (error instanceof Error) return error.message;
+
   return defaultMsg;
 }
