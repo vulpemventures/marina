fd0458ac680c317f68bf9130c4632111564e03a2
Louis <louis@vulpem.com>
2022-03-16 15:12:02 +0100

    fix descriptors test

--- test/descriptors.spec.ts
+++ test/descriptors.spec.ts
@@ -1,7 +1,7 @@
 import { script } from 'liquidjs-lib';
-import { compile, NodeType } from '../src/descriptors/ast';
-import { parseScript } from '../src/descriptors/parser';
-import type { Context} from '../src/descriptors/preprocessing';
+import { compile, ScriptType, TypeAST } from '../src/descriptors/ast';
+import { parseSCRIPT } from '../src/descriptors/parser';
+import type { Context } from '../src/descriptors/preprocessing';
 import { preprocessor } from '../src/descriptors/preprocessing';
 import { evaluate } from '../src/descriptors';
 import { toXpub, bip32 } from 'ldk';
@@ -21,7 +21,7 @@ describe('evaluate', () => {
 
     expect(processedText).toEqual(`asm(OP_DUP OP_HASH160 ${key} OP_EQUALVERIFY OP_CHECKSIG)`);
     const result = evaluate(ctx, text);
-    expect(result.redeemScript).toEqual(
+    expect(result.scriptPubKey()).toEqual(
       script.fromASM(`OP_DUP OP_HASH160 ${key} OP_EQUALVERIFY OP_CHECKSIG`)
     );
   });
@@ -30,26 +30,14 @@ describe('evaluate', () => {
 describe('parser', () => {
   it('should parse raw template', () => {
     const text = 'raw(08e53a2e9e3ed3ba34dd5ce7f94e1e62abc3549e2d8796d8cd01102a23af1a)';
-    const [tree, remainingText] = parseScript(text);
+    const [tree, remainingText] = parseSCRIPT(text);
 
     expect(tree).toEqual({
-      type: NodeType.RAW,
-      value: '08e53a2e9e3ed3ba34dd5ce7f94e1e62abc3549e2d8796d8cd01102a23af1a',
-      children: [],
-    });
-    expect(remainingText).toEqual('');
-  });
-
-  it('should parse elp2wsh template', () => {
-    const text = 'elp2wsh(raw(08e53a2e9e3ed3ba34dd5ce7f94e1e62abc3549e2d8796d8cd01102a23af1a))';
-    const [tree, remainingText] = parseScript(text);
-
-    expect(tree).toEqual({
-      type: NodeType.ELP2WSH,
-      value: undefined,
+      type: TypeAST.SCRIPT,
+      value: ScriptType.RAW,
       children: [
         {
-          type: NodeType.RAW,
+          type: TypeAST.HEX,
           value: '08e53a2e9e3ed3ba34dd5ce7f94e1e62abc3549e2d8796d8cd01102a23af1a',
           children: [],
         },
@@ -60,31 +48,43 @@ describe('parser', () => {
 
   it('should parse eltr template', () => {
     const text =
-      'eltr(c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5,{raw(fff97bd5755eeea420453a14355235d382f6472f8568a18b2f057a1460297556),raw(e493dbf1c10d80f3581e4904930b1404cc6c13900ee0758474fa94abe8c4cd13)})';
-    const [tree, remainingText] = parseScript(text);
+      'eltr(c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5,{asm(c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5 OP_CHECKSIG),asm(d01115d548e7561b15c38f004d734633687cf4419620095bc5b0f47070afe85a OP_CHECKSIG)})';
+    const [tree, remainingText] = parseSCRIPT(text);
 
     expect(tree).toEqual({
-      type: NodeType.ELTR,
-      value: undefined,
+      type: TypeAST.SCRIPT,
+      value: ScriptType.ELTR,
       children: [
         {
-          type: NodeType.KEY,
+          type: TypeAST.KEY,
           value: 'c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5',
           children: [],
         },
         {
-          type: NodeType.TREE_NODE,
+          type: TypeAST.TREE,
           value: undefined,
           children: [
             {
-              type: NodeType.RAW,
-              value: 'fff97bd5755eeea420453a14355235d382f6472f8568a18b2f057a1460297556',
-              children: [],
+              type: TypeAST.SCRIPT,
+              value: ScriptType.ASM,
+              children: [
+                {
+                  type: TypeAST.HEX,
+                  value: '20c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5ac',
+                  children: [],
+                },
+              ],
             },
             {
-              type: NodeType.RAW,
-              value: 'e493dbf1c10d80f3581e4904930b1404cc6c13900ee0758474fa94abe8c4cd13',
-              children: [],
+              type: TypeAST.SCRIPT,
+              value: ScriptType.ASM,
+              children: [
+                {
+                  type: TypeAST.HEX,
+                  value: '20d01115d548e7561b15c38f004d734633687cf4419620095bc5b0f47070afe85aac',
+                  children: [],
+                },
+              ],
             },
           ],
         },
@@ -93,40 +93,26 @@ describe('parser', () => {
     expect(remainingText).toEqual('');
 
     const res = compile(tree!);
-    expect(res.redeemScript.toString('hex')).toEqual(
-      '51200ef0975c80e0ff6cc3ef4c02944329bb9cea26c54db74ff1919bb3dfb75b980f'
+    expect(res.scriptPubKey().toString('hex')).toEqual(
+      '5120ec2e5b3649abf837992e92c55361ed2089a633cb69b540e90a0d098c88f0891f'
     );
-    expect(res.getWitnesses).toBeDefined();
+    expect(res.witnesses).toBeDefined();
+    expect(res.taprootHashTree).toBeDefined();
     expect(
-      res.getWitnesses!('fff97bd5755eeea420453a14355235d382f6472f8568a18b2f057a1460297556').map(
+      res.witnesses!('20d01115d548e7561b15c38f004d734633687cf4419620095bc5b0f47070afe85aac').map(
         (b) => b.toString('hex')
       )
     ).toEqual([
-      'fff97bd5755eeea420453a14355235d382f6472f8568a18b2f057a1460297556',
-      'c4c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5978752a16e4be9a633eaa7eb45580a6a2470aefe536ef541348df57186dbefd5',
+      '20d01115d548e7561b15c38f004d734633687cf4419620095bc5b0f47070afe85aac',
+      'c4c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee52a0a73facb720b9fdea8938ab1315cd66caf024eaa3c18ed79e7491bc3959767',
     ]);
-
     expect(
-      res.getWitnesses!('e493dbf1c10d80f3581e4904930b1404cc6c13900ee0758474fa94abe8c4cd13').map(
+      res.witnesses!('20c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5ac').map(
         (b) => b.toString('hex')
       )
     ).toEqual([
-      'e493dbf1c10d80f3581e4904930b1404cc6c13900ee0758474fa94abe8c4cd13',
-      'c4c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee55bfb6773261ad663908d570c3e53f529f3ec9b1ad8eefa9cfbc353023c2e01de',
+      '20c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5ac',
+      'c4c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5da5e27c17a7d951b00e01d1ad070d5f32175dec2511390644ae3b6caca14338a',
     ]);
-  });
-
-  it('should parse asm template', () => {
-    const text =
-      'asm(c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5 OP_CHECKSIG)';
-    const [tree, remainingText] = parseScript(text);
-    expect(tree).toEqual({
-      type: NodeType.ASM,
-      value: script
-        .fromASM('c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5 OP_CHECKSIG')
-        .toString('hex'),
-      children: [],
-    });
-    expect(remainingText).toEqual('');
   });
 });
