b63020c127debdb61056022c7199c6dfe874fd3b
Louis <louis@vulpem.com>
2022-05-05 13:20:56 +0200

    mangle reserved Buffer with TerserPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,30 +1,21 @@
 // @ts-nocheck
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyEsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimize: true,
     minimizer: [
-      new UglifyEsPlugin({
-        uglifyOptions: {
+      new TerserPlugin({
+        terserOptions: {
+          ecma: 6,
           mangle: {
-            reserved: [
-              'Buffer',
-              'BigInteger',
-              'Point',
-              'ECPubKey',
-              'ECKey',
-              'sha512_asm',
-              'asm',
-              'ECPair',
-              'HDNode'
-            ]
-          }
-        }
-      })
+            reserved: ['Buffer']
+          },
+        },
+      }),
     ],
   },
 });
