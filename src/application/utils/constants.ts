414ecfd9549bc130faa553c1ca72d7ffc2676d9a
João Bordalo <joao.bordalo@gmail.com>
2022-05-09 15:57:02 +0100

    featured asset icons are now served locally

--- src/application/utils/constants.ts
+++ src/application/utils/constants.ts
@@ -10,44 +10,50 @@ export const feeLevelToSatsPerByte: { [key: string]: number } = {
   '100': 0.1,
 };
 
+const getLocalImagePath = (asset: string) => `/assets/images/liquid-assets/${asset}`;
+
 // featured assets
 const featuredAssets = {
-  usdt: {
-    liquid: 'ce091c998b83c78bb71a632313ba3760f1763d9cfcffae02258ffa9865a37bd2',
-    testnet: 'f3d1ec678811398cd2ae277cbe3849c6f6dbd72c74bc542f7c4b11ff0e820958',
+  lbtc: {
+    mainnet: networks.liquid.assetHash,
+    testnet: networks.testnet.assetHash,
+    regtest: networks.regtest.assetHash,
   },
   lcad: {
-    liquid: '0e99c1a6da379d1f4151fb9df90449d40d0608f6cb33a5bcbfc8c265f42bab0a',
+    mainnet: '0e99c1a6da379d1f4151fb9df90449d40d0608f6cb33a5bcbfc8c265f42bab0a',
     testnet: 'ac3e0ff248c5051ffd61e00155b7122e5ebc04fd397a0ecbdd4f4e4a56232926',
+  },
+  usdt: {
+    mainnet: 'ce091c998b83c78bb71a632313ba3760f1763d9cfcffae02258ffa9865a37bd2',
+    testnet: 'f3d1ec678811398cd2ae277cbe3849c6f6dbd72c74bc542f7c4b11ff0e820958',
   },
 };
 
-// featured assets map
-// translates testnet and regtest asset hashes to mainnet asset hash
+// featured assets map: from an asset hash, get local image path
 const featuredAssetsMap = new Map<string, string>();
-featuredAssetsMap.set(networks.testnet.assetHash, networks.liquid.assetHash);
-featuredAssetsMap.set(networks.regtest.assetHash, networks.liquid.assetHash);
-featuredAssetsMap.set(featuredAssets.usdt.testnet, featuredAssets.usdt.liquid);
-featuredAssetsMap.set(featuredAssets.lcad.testnet, featuredAssets.lcad.liquid);
+featuredAssetsMap.set(featuredAssets.lbtc.mainnet, getLocalImagePath('lbtc.png'));
+featuredAssetsMap.set(featuredAssets.lbtc.testnet, getLocalImagePath('lbtc.png'));
+featuredAssetsMap.set(featuredAssets.lbtc.regtest, getLocalImagePath('lbtc.png'));
+featuredAssetsMap.set(featuredAssets.usdt.mainnet, getLocalImagePath('usdt.png'));
+featuredAssetsMap.set(featuredAssets.usdt.testnet, getLocalImagePath('usdt.png'));
+featuredAssetsMap.set(featuredAssets.lcad.mainnet, getLocalImagePath('lcad.png'));
+featuredAssetsMap.set(featuredAssets.lcad.testnet, getLocalImagePath('lcad.png'));
 
-// given an asset hash, return url for image path
-const getImagePath = (hash: string): string => {
-  const remoteServer = 'https://liquid.network/api/v1/asset';
-  return `${remoteServer}/${hash}/icon`;
-};
+// given an asset hash, return url for image path from mempool
+const getRemoteImagePath = (hash: string) => `https://liquid.network/api/v1/asset/${hash}/icon`;
 
 // getter function using to look for assets on testnet and regtest
 // and return the correct asset icon path (with asset hash from mainnet)
 export function getAssetImage(assetHash: string): string {
-  const mainnetHash = featuredAssetsMap.get(assetHash);
-  if (mainnetHash) return getImagePath(mainnetHash);
-  return getImagePath(assetHash);
+  const localImagePath = featuredAssetsMap.get(assetHash);
+  if (localImagePath) return localImagePath;
+  return getRemoteImagePath(assetHash);
 }
 
 export function onErrorImg(event: any): void {
-  const imgPath = '/assets/images/question-mark.svg';
+  const unknownImgPath = getLocalImagePath('unknown.svg');
   event.currentTarget.onerror = null; // prevents looping
-  event.currentTarget.src = imgPath;
+  event.currentTarget.src = unknownImgPath;
 }
 
 export const defaultPrecision = 8;
