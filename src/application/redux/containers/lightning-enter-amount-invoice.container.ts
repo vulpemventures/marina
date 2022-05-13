31e55ce0e304101f31276201733df9f5b6e2a6dc
João Bordalo <joao.bordalo@gmail.com>
2022-05-13 18:01:54 +0100

    removes unused props

--- src/application/redux/containers/lightning-enter-amount-invoice.container.ts
+++ src/application/redux/containers/lightning-enter-amount-invoice.container.ts
@@ -1,18 +1,14 @@
 import { connect } from 'react-redux';
-import { MainAccountID } from '../../../domain/account';
 import { RootReducerState } from '../../../domain/common';
 import LightningAmountInvoiceView, {
   LightningAmountInvoiceProps,
 } from '../../../presentation/wallet/receive/lightning-enter-amount-invoice';
 import { selectEsploraURL } from '../selectors/app.selector';
-import { selectMainAccount, selectUtxos } from '../selectors/wallet.selector';
 
 const mapStateToProps = (state: RootReducerState): LightningAmountInvoiceProps => {
   return {
     network: state.app.network,
-    account: selectMainAccount(state),
     explorerURL: selectEsploraURL(state),
-    utxos: selectUtxos(MainAccountID)(state),
   };
 };
 
