927d84c9305d95a92b990fac4bd79799fbde2e6b
João Bordalo <joao.bordalo@gmail.com>
2022-05-30 15:20:10 +0100

    refactor lightning-result.tsx: hide qrcode after successful invoice payment

--- src/presentation/wallet/receive/lightning-result.tsx
+++ src/presentation/wallet/receive/lightning-result.tsx
@@ -28,41 +28,55 @@ const LightningResultView = ({
     );
   };
 
-  const Result = ({ text }: { text: string }) => (
-    <p className="mb-4 text-xs font-medium whitespace-pre">{text}</p>
+  const ShowResult = () =>
+    lookingForPayment && !isSet(txID) ? (
+      <p className="mb-4 text-xs font-medium whitespace-pre">⏳ Waiting for payment...</p>
+    ) : !lookingForPayment && isSet(txID) ? (
+      <p className="my-auto text-xs font-medium whitespace-pre">✅ Invoice paid!</p>
+    ) : null;
+
+  const AuxiliarButton = ({ children }: { children: React.ReactNode }) => (
+    <button className="text-primary focus:outline-none text-xs font-medium" onClick={handleExpand}>
+      {children}
+    </button>
   );
 
-  return (
-    <div className="w-80 h-96 rounded-4xl flex flex-col items-center justify-between px-10 py-4 mx-auto bg-white">
-      {lookingForPayment && !isSet(txID) ? (
-        <Result text="⏳  Waiting for payment..." />
-      ) : !lookingForPayment && isSet(txID) ? (
-        <Result text="✅  Invoice paid!" />
-      ) : null}
+  const ShowQrcode = () =>
+    lookingForPayment && !isInvoiceExpanded ? (
+      <>
+        <QRCode size={176} value={invoice.toLowerCase()} />
+        <p className="font-regular mt-4 text-lg">{formatAddress(invoice)}</p>
+        <AuxiliarButton>Expand</AuxiliarButton>
+      </>
+    ) : null;
 
-      {isInvoiceExpanded ? (
+  const ShowExpanded = () =>
+    lookingForPayment && isInvoiceExpanded ? (
+      <>
         <p className="text-xs font-medium break-all">{invoice}</p>
-      ) : (
-        <>
-          <QRCode size={176} value={invoice.toLowerCase()} />
-          <p className="font-regular mt-4 text-lg">{formatAddress(invoice)}</p>
-        </>
-      )}
+        <AuxiliarButton>Show QR Code</AuxiliarButton>
+      </>
+    ) : null;
 
-      <button
-        className="text-primary focus:outline-none text-xs font-medium"
-        onClick={handleExpand}
-      >
-        {isInvoiceExpanded ? 'Show QR code' : 'Expand'}
-      </button>
+  const ShowErrors = () =>
+    isSet(errors.submit) ? (
+      <p className="text-red mt-1 text-xs font-medium text-left">{errors.submit}</p>
+    ) : null;
 
-      {isSet(errors.submit) && (
-        <p className="text-red mt-1 text-xs font-medium text-left">{errors.submit}</p>
-      )}
-
+  const ShowCopyButton = () =>
+    lookingForPayment ? (
       <Button className="w-3/5 mt-4" onClick={handleCopy}>
         <span className="text-base antialiased font-bold">{buttonText}</span>
       </Button>
+    ) : null;
+
+  return (
+    <div className="w-80 h-96 rounded-4xl flex flex-col items-center justify-between px-10 py-4 mx-auto bg-white">
+      <ShowResult />
+      <ShowQrcode />
+      <ShowExpanded />
+      <ShowErrors />
+      <ShowCopyButton />
     </div>
   );
 };
