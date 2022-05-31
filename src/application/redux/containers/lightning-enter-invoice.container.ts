fd9c20c5325befb6660de37de6640c76f1864fb5
João Bordalo <joao.bordalo@gmail.com>
2022-05-31 14:59:34 +0100

    lightning-enter-invoice container returns lbtcBalance

--- src/application/redux/containers/lightning-enter-invoice.container.ts
+++ src/application/redux/containers/lightning-enter-invoice.container.ts
@@ -4,13 +4,14 @@ import { RootReducerState } from '../../../domain/common';
 import LightningInvoiceView, {
   LightningInvoiceProps,
 } from '../../../presentation/wallet/send/lightning-enter-invoice';
+import { lbtcAssetByNetwork } from '../../utils/network';
 import { selectBalances } from '../selectors/balance.selector';
 
 const mapStateToProps = (state: RootReducerState): LightningInvoiceProps => {
-  return {
-    balances: selectBalances(MainAccountID)(state),
-    network: state.app.network,
-  };
+  const network = state.app.network;
+  const balances = selectBalances(MainAccountID)(state);
+  const lbtcBalance = balances[lbtcAssetByNetwork(network)];
+  return { lbtcBalance, network };
 };
 
 const LightningInvoice = connect(mapStateToProps)(LightningInvoiceView);
