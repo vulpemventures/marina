bffe2351bede50c11c1b27f1db493680e1ff47f3
Louis Singer <41042567+louisinger@users.noreply.github.com>
2022-08-19 20:09:33 +0200

    naming fix + select only spendableByMarina accounts in UI (#376)
    * naming fix + select only spendableByMarina accounts in UI
    
    * Apply suggestions from code review
    
    Co-authored-by: João Bordalo <bordalix@users.noreply.github.com>
    
    Co-authored-by: Marco Argentieri <3596602+tiero@users.noreply.github.com>
    Co-authored-by: João Bordalo <bordalix@users.noreply.github.com>

--- src/application/redux/containers/transactions.container.ts
+++ src/application/redux/containers/transactions.container.ts
@@ -3,11 +3,14 @@ import type { RootReducerState } from '../../../domain/common';
 import type { TransactionsProps } from '../../../presentation/wallet/transactions';
 import TransactionsView from '../../../presentation/wallet/transactions';
 import { selectElectrsURL, selectNetwork } from '../selectors/app.selector';
-import { selectAllAccountsIDs, selectTransactions } from '../selectors/wallet.selector';
+import {
+  selectAllAccountsIDsSpendableViaUI,
+  selectTransactions,
+} from '../selectors/wallet.selector';
 
 const mapStateToProps = (state: RootReducerState): TransactionsProps => ({
   assets: state.assets,
-  transactions: selectTransactions(...selectAllAccountsIDs(state))(state),
+  transactions: selectTransactions(...selectAllAccountsIDsSpendableViaUI(state))(state),
   webExplorerURL: selectElectrsURL(state),
   network: selectNetwork(state),
   isWalletVerified: state.wallet.isVerified,
