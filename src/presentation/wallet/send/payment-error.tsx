f7f9c0ff1d34b025624a4926b832505c407d8caf
Louis Singer <41042567+louisinger@users.noreply.github.com>
2022-09-27 14:33:46 +0200

    payment-error page: RETRY button in case of INVALID_PASSWORD_ERROR (#415)
    * retry btn in every case
    
    * handle INVALID_PASSWORD error during modal unlock
    
    * error view refacto

--- src/presentation/wallet/send/payment-error.tsx
+++ src/presentation/wallet/send/payment-error.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router';
 import ShellPopUp from '../../components/shell-popup';
 import Button from '../../components/button';
-import {
-  INVALID_PASSWORD_ERROR,
-  SOMETHING_WENT_WRONG_ERROR,
-} from '../../../application/utils/constants';
+import { SOMETHING_WENT_WRONG_ERROR } from '../../../application/utils/constants';
 import { SEND_CONFIRMATION_ROUTE, SEND_PAYMENT_SUCCESS_ROUTE } from '../../routes/constants';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectEsploraURL } from '../../../application/redux/selectors/app.selector';
@@ -31,7 +28,7 @@ const PaymentError: React.FC = () => {
     if (!txid) throw new Error('something went wrong with the tx broadcasting');
     // lock utxos used in successful broadcast
     for (const utxo of state.selectedUtxos) {
-      void dispatch(lockUtxo(utxo));
+      await dispatch(lockUtxo(utxo));
     }
     // navigate to payment success page
     history.push({
@@ -56,11 +53,9 @@ const PaymentError: React.FC = () => {
       <h1 className="mt-8 text-lg font-medium">{SOMETHING_WENT_WRONG_ERROR}</h1>
       <p className="font-small mt-4 text-sm break-all">{state?.error}</p>
       <img className="my-14 mx-auto" src="/assets/images/cross.svg" alt="error" />
-      {state?.error !== INVALID_PASSWORD_ERROR && (
-        <Button className="w-36 container mx-auto mt-10" onClick={handleRetry} textBase={true}>
-          Retry
-        </Button>
-      )}
+      <Button className="w-36 container mx-auto mt-10" onClick={handleRetry} textBase={true}>
+        Retry
+      </Button>
     </ShellPopUp>
   );
 };
