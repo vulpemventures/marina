7f227879b70f3271f94e31df8b2be9bf636c3482
Louis <louis@vulpem.com>
2023-04-12 18:09:25 +0200

    rework UI

--- src/extension/popups/sign-pset.tsx
+++ src/extension/popups/sign-pset.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import zkpLib from '@vulpemventures/secp256k1-zkp';
 import Button from '../components/button';
 import ShellConnectPopup from '../components/shell-connect-popup';
 import ModalUnlock from '../components/modal-unlock';
@@ -11,91 +10,18 @@ import {
 } from '../../infrastructure/storage/common';
 import { SignerService } from '../../application/signer';
 import { popupResponseMessage } from '../../domain/message';
-import { AssetHash, ElementsValue, Pset } from 'liquidjs-lib';
+import { Pset } from 'liquidjs-lib';
 import { useBackgroundPortContext } from '../context/background-port-context';
 import { useStorageContext } from '../context/storage-context';
-import type { ScriptDetails, UnblindedOutput } from 'marina-provider';
 import { useToastContext } from '../context/toast-context';
 import { extractErrorMessage } from '../utility/error';
-import type { AppRepository, AssetRepository, WalletRepository } from '../../domain/repository';
-import { WalletRepositoryUnblinder } from '../../application/unblinder';
 import { fromSatoshi } from '../utility';
 import { Spinner } from '../components/spinner';
+import type { TxDetailsExtended } from '../../domain/transaction';
+import { computeTxDetailsExtended } from '../../domain/transaction';
+import { MainAccount, MainAccountLegacy, MainAccountTest } from '../../application/account';
 
-interface PsetInfos {
-  txID: string;
-  utxosInInputs: UnblindedOutput[];
-  walletOutputs: ({ asset: string; value: number } & ScriptDetails)[];
-  feeOutputAmount: number;
-}
-
-async function getPsetInformations(
-  psetb64: string,
-  appRepository: AppRepository,
-  walletRepository: WalletRepository,
-  assetRepository: AssetRepository
-): Promise<PsetInfos> {
-  const pset = Pset.fromBase64(psetb64);
-  const network = await appRepository.getNetwork();
-  if (!network) throw new Error('unknown network');
-  const unsignedTx = pset.unsignedTx();
-
-  const result: PsetInfos = {
-    txID: unsignedTx.getId(),
-    utxosInInputs: [],
-    walletOutputs: [],
-    feeOutputAmount: 0,
-  };
-
-  const inputsBlindingData = await walletRepository.getOutputBlindingData(
-    ...pset.inputs.map((input) => ({
-      txid: Buffer.from(input.previousTxid).reverse().toString('hex'),
-      vout: input.previousTxIndex,
-    }))
-  );
-
-  result.utxosInInputs = inputsBlindingData;
-
-  const allWalletScripts = await walletRepository.getAccountScripts(network);
-  const unblinder = new WalletRepositoryUnblinder(
-    walletRepository,
-    appRepository,
-    assetRepository,
-    await zkpLib()
-  );
-
-  for (const output of unsignedTx.outs) {
-    if (!output.script || output.script.length === 0) {
-      result.feeOutputAmount = ElementsValue.fromBytes(output.value).number; // fee output is always unconfidential
-      continue;
-    }
-
-    const script = Buffer.from(output.script).toString('hex');
-    const scriptDetails = allWalletScripts[script];
-    if (scriptDetails) {
-      const value = ElementsValue.fromBytes(output.value);
-      if (value.isConfidential) {
-        const [unblinded] = await unblinder.unblind(output);
-        if (unblinded instanceof Error) continue;
-        result.walletOutputs.push({
-          ...scriptDetails,
-          asset: unblinded.asset,
-          value: unblinded.value,
-        });
-      } else {
-        result.walletOutputs.push({
-          ...scriptDetails,
-          asset: AssetHash.fromBytes(output.asset).hex,
-          value: value.number,
-        });
-      }
-    }
-  }
-
-  return result;
-}
-
-const PsetView: React.FC<PsetInfos> = ({ txID, utxosInInputs, walletOutputs, feeOutputAmount }) => {
+const PsetView: React.FC<TxDetailsExtended> = ({ txFlow }) => {
   const { cache } = useStorageContext();
   const getPrecision = (asset: string) => {
     if (!cache || !cache.assetsDetails || !cache.assetsDetails.value[asset]) return 8;
@@ -114,28 +40,40 @@ const PsetView: React.FC<PsetInfos> = ({ txID, utxosInInputs, walletOutputs, fee
     <div className="flex flex-col">
       <p className="mt-4 text-base font-medium text-center">Requests you to send</p>
       <div className="container flex flex-col">
-        {walletOutputs.map(({ asset, value, accountName }, index) => (
-          <div key={index}>
-            <div className="container flex justify-between mt-6">
-              <span className="text-lg font-medium">
-                {fromSatoshi(value, getPrecision(asset))}{' '}
-              </span>
-              <span className="text-lg font-medium">{getTicker(asset)}</span>
+        {Object.entries(txFlow)
+          .filter(([, value]) => value > 0)
+          .map(([asset, value], index, array) => (
+            <div key={index}>
+              <div className="container flex justify-between mt-6">
+                <span className="text-lg font-medium">
+                  {fromSatoshi(value, getPrecision(asset))}{' '}
+                </span>
+                <span className="text-lg font-medium">{getTicker(asset)}</span>
+              </div>
+              {index < array.length - 1 && (
+                <div className="w-48 mx-auto border-b-0.5 border-primary pt-1.5" />
+              )}
             </div>
-            <div className="container flex items-baseline justify-between">
-              <span className="mr-2 text-lg font-medium">To: </span>
-              <span className="font-small text-sm break-all">{accountName}</span>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
-      <p className="mt-2 text-base font-medium">
-        Requests you to sign {utxosInInputs.filter(({ blindingData }) => !!blindingData).length}{' '}
-        utxo(s)
-      </p>
-      <p className="mt-2 text-base font-medium">
-        Fee output: {fromSatoshi(feeOutputAmount, 8)} L-BTC
-      </p>
+      <p className="mt-4 text-base font-medium text-center">Requests you to spend</p>
+      <div className="container flex flex-col">
+        {Object.entries(txFlow)
+          .filter(([, value]) => value < 0)
+          .map(([asset, value], index, array) => (
+            <div key={index}>
+              <div className="container flex justify-between mt-6">
+                <span className="text-lg font-medium">
+                  {fromSatoshi(Math.abs(value), getPrecision(asset))}{' '}
+                </span>
+                <span className="text-lg font-medium">{getTicker(asset)}</span>
+              </div>
+              {index < array.length - 1 && (
+                <div className="w-48 mx-auto border-b-0.5 border-primary pt-1.5" />
+              )}
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
@@ -146,13 +84,13 @@ export interface SignTransactionPopupResponse {
 }
 
 const ConnectSignTransaction: React.FC = () => {
-  const { walletRepository, appRepository, assetRepository } = useStorageContext();
+  const { walletRepository, appRepository } = useStorageContext();
   const { showToast } = useToastContext();
   const { backgroundPort } = useBackgroundPortContext();
 
   const [isModalUnlockOpen, showUnlockModal] = useState<boolean>(false);
   const [error, setError] = useState<string>();
-  const [psetInfos, setPsetInfos] = useState<PsetInfos>();
+  const [txDetails, setTxDetails] = useState<TxDetailsExtended>();
 
   const psetToSign = useSelectPopupPsetToSign();
   const hostname = useSelectPopupHostname();
@@ -163,9 +101,23 @@ const ConnectSignTransaction: React.FC = () => {
   useEffect(() => {
     const init = async () => {
       if (!psetToSign) return;
-      setPsetInfos(
-        await getPsetInformations(psetToSign, appRepository, walletRepository, assetRepository)
+      const network = await appRepository.getNetwork();
+      if (!network) throw new Error('unknown network');
+      const pset = Pset.fromBase64(psetToSign);
+
+      const mainAccountsScripts = await walletRepository.getAccountScripts(
+        network,
+        MainAccountLegacy,
+        network === 'liquid' ? MainAccount : MainAccountTest
       );
+
+      const txDetailsExtended = await computeTxDetailsExtended(
+        appRepository,
+        walletRepository,
+        mainAccountsScripts
+      )({ height: -1, hex: pset.unsignedTx().toHex() });
+
+      setTxDetails(txDetailsExtended);
     };
     init().catch((e) => {
       console.error(e);
@@ -214,13 +166,13 @@ const ConnectSignTransaction: React.FC = () => {
         <>
           <h1 className="mt-8 text-2xl font-medium text-center break-all">{hostname}</h1>
 
-          {!psetInfos ? (
+          {!txDetails ? (
             <div className="flex flex-col items-center mt-8">
               <Spinner />
               <p className="font-medium">Loading PSET data...</p>
             </div>
           ) : (
-            <PsetView {...psetInfos} />
+            <PsetView {...txDetails} />
           )}
 
           <ButtonsAtBottom>
