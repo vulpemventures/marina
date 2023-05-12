2dad469a2d9d464a5b908583c08374e2c28a5dc5
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2023-05-13 01:36:12 +0200

    FUSD upatde icon and asset ID (#475)
    * Delete fusd.png
    
    * Add files via upload
    
    * Update constants.ts
    
    * Add files via upload
    
    * Delete FUSD_logo.png

--- src/domain/constants.ts
+++ src/domain/constants.ts
@@ -22,6 +22,7 @@ const featuredAssets = {
     testnet: 'f3d1ec678811398cd2ae277cbe3849c6f6dbd72c74bc542f7c4b11ff0e820958',
   },
   fusd: {
+    mainnet: '0dea022a8a25abb128b42b0f8e98532bc8bd74f8a77dc81251afcc13168acef7',
     testnet: '0d86b2f6a8c3b02a8c7c8836b83a081e68b7e2b4bcdfc58981fc5486f59f7518',
   },
 };
@@ -36,6 +37,7 @@ featuredAssetsMap.set(featuredAssets.usdt.testnet, getLocalImagePath('usdt.png')
 featuredAssetsMap.set(featuredAssets.lcad.mainnet, getLocalImagePath('lcad.png'));
 featuredAssetsMap.set(featuredAssets.lcad.testnet, getLocalImagePath('lcad.png'));
 featuredAssetsMap.set(featuredAssets.fusd.testnet, getLocalImagePath('fusd.png'));
+featuredAssetsMap.set(featuredAssets.fusd.mainnet, getLocalImagePath('fusd.png'));
 
 export const FEATURED_ASSETS = Array.from(featuredAssetsMap.keys());
 
