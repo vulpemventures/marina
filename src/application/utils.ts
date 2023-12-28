ba5f83ec87105be4998853e7796f6a75fe4c0b08
João Bordalo <bordalix@users.noreply.github.com>
2023-12-28 17:04:23 +0000

    New utils function swapEndian
    Co-authored-by: Marco Argentieri <tiero@users.noreply.github.com>

--- src/application/utils.ts
+++ src/application/utils.ts
@@ -1,3 +1,7 @@
 export function h2b(hex: string): Buffer {
   return Buffer.from(hex, 'hex');
 }
+
+export function swapEndian(hex: string): string {
+  return Buffer.from(hex, 'hex').reverse().toString('hex');
+}
